23ccb4d7b7710a16403b9c1b613220f2220d6c3e
Leandro Rezende <lrezendepinheiro@gmail.com>
2019-03-26 21:35:20 -0300

    Spread Operators, pushing on array and receive a lot of arguments on a function

---
+++ spreadOperator.js
@@ -0,0 +1,22 @@
+// Spread operator examplesss letsssssss goooooooooooooooooooo
+
+//concating begining and end of an array
+
+let arrayAges = [13,23,63,132];
+
+let moreAgesFinalArray = [12,232,...arrayAges];
+let moreAgesBeginigArray = [...arrayAges, 12,35,23]
+
+console.log("moreAgesFinalArray" + moreAgesFinalArray);
+console.log("moreAgesBeginigArray" + moreAgesBeginigArray);
+
+
+//receving multiple values on a function
+
+const sumAll = function sumAll(...numbers){
+                return numbers.reduce(function(prev, num){
+                    return prev+ num
+                },0);
+};
+
+console.log(sumAll(1,2,3));
