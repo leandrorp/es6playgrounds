bf2235c2b5a08a78b6cd0e2a6662890f26496114
Leandro <lrezendepinheiro@gmail.com>
2019-09-12 17:19:41 -0300

    Working with big int and flat array

--- googleIO2019.js
+++ googleIO2019.js
@@ -1,3 +1,5 @@
+//Internationalization API
+
 let trlPorutuguesBR = new Intl.RelativeTimeFormat('pt-BR', {
     numeric: 'auto'
   });
@@ -9,7 +11,19 @@ let trlPorutuguesBR = new Intl.RelativeTimeFormat('pt-BR', {
     type: 'disjunction'
   });
   var list = [ 'manzanas', 'mangos', 'plátanos' ];
-  trlPorutuguesBRList.format( list )  // manzanas, mangos ou plátanos
+  trlPorutuguesBRList.format( list )  // manzanas, mangos OU plátanos
   
+  //BigInts
+
+  let multiply = 100000000000009 * 123;
+   // return wrong result
+
+   let b = 10000000000000009n * 123n;
+   // now it works
+
+   const array = [1,2,3,4,1,2,[3,[4]]];
+   array.flat(); // remove the one level of arrays [1,2,3,4,1,2,3,[4]]
+   array.flat(Infinity)   // remove all levels   [1,2,3,4,1,2,3,4]
+   
   
-  
+
